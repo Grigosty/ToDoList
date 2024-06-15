d8ca83834c8555f1e6deb9831514567522fdc6bc
Grigosty <122815869+Grigosty@users.noreply.github.com>
2024-06-15 14:32:31 +0300

    Проработана логика авторизации и аутентификации, настроено получение всез списков пользователя

---
+++ pkg/handler/list.go
@@ -0,0 +1,67 @@
+package handler
+
+import (
+	"net/http"
+
+	todo "github.com/Grigosty/ToDoList"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) createList(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err!=nil {
+		newErrorResponse(c,http.StatusInternalServerError,"user id not found")
+		return
+	}
+
+	var input todo.TodoList
+	if err := c.BindJSON(&input); err!=nil{
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id,err := h.services.TodoList.Create(userId,input)
+	if err!=nil{
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id":id,
+	})
+
+}
+
+type getAllListsResponse struct {
+	Data []todo.TodoList `json: "data"`
+}
+
+func (h *Handler) getAllLists(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err!=nil {
+		newErrorResponse(c,http.StatusInternalServerError,"user id not found")
+		return
+	}
+
+	lists,err := h.services.TodoList.GetAll(userId)
+	if err!=nil{
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, getAllListsResponse{
+		Data: lists,
+	})
+}
+
+func (h *Handler) getListById(c *gin.Context) {
+
+}
+
+func (h *Handler) updateList(c *gin.Context){
+	
+}
+
+func (h *Handler) deleteList(c *gin.Context){
+	
+}
---
+++ pkg/repository/repository.go
@@ -0,0 +1,32 @@
+package repository
+
+import (
+	todo "github.com/Grigosty/ToDoList"
+	"github.com/jmoiron/sqlx"
+)
+
+type Authorization interface {
+	CreateUser(user todo.User) (int,error)
+	GetUser(username,password string) (todo.User,error)
+}
+
+type TodoList interface { 
+	Create(userId int, list todo.TodoList)(int,error)
+	GetAll(userId int)([]todo.TodoList, error)
+}
+
+type TodoItem interface {
+}
+
+type Repository struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewRepository(db *sqlx.DB) *Repository {
+	return &Repository{
+		Authorization: NewAuthPostgres(db),
+		TodoList: NewTodoListPostgres(db),
+	}
+}
